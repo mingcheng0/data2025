bba6c3ea9fa19268261d971d7fac3e46f341dec4
mingcheng0 <ming0962091542@gmail.com>
2025-09-23 16:04:18 +0800

    20250923

--- node.js
+++ node.js
@@ -1,4 +1,13 @@
-var weight=50;
-var height=160;
+const readline = require('readline-sync')
+var readlineSync = require('readline-sync')
+
+var weight=0;
+while(weight<10 || weight>120){
+ weight=readline.questionInt("your wight in kg:");
+}
+var height=0
+while(height<50 || height>250){
+ height=readline.questionFloat("your height in cm:")
+}
 var bmi = weight/((height/100)**2);
 console.log("Hello! Your BMI value is "+ bmi);
